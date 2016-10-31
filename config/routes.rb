3afd769b57f671e57853a47b8cf15d08e8b53eae
Benedikt Kienzler <mail@benedikt-kienzler.com>
2016-10-31 18:07:24 +0100

    Add devise for user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :bookmarks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
+Bookmarks::Application.routes.draw do
+# […]
+root ‘bookmarks#index’
+
+# […]
 end
